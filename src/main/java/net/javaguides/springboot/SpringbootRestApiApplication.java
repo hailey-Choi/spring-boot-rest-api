99806a030299f200943cadd6f6df08162bf85eb8
Hailey Choi <tngus254@hku.hk>
2023-06-09 15:37:48 +0900

    add student controller

--- src/main/java/net/javaguides/springboot/SpringbootRestApiApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootRestApiApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// is a combination of ...
+// @SpringBootConfiguration : allows to resister extra beans in the context or import additional configuration classes (Java-based configuration)
+// @EnableAutoConfiguration : enable Spring Boot's auto-configuration mechanism
+// @ComponentScan : enable @Component scan on the package where the application is located
 public class SpringbootRestApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -0,0 +1,20 @@
+package net.javaguides.springboot.controller;
+
+import net.javaguides.springboot.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    // http://localhost:8080/student
+    @GetMapping("student")
+    public Student getStudent() {
+        Student student = new Student(
+                1,
+                "Hailey",
+                "Choi"
+        );
+        return student;
+    }
+}
